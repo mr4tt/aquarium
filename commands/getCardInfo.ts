a2af8ef733253d67df97eb3dd726e44ea4162bcb
mrat <78581772+mr4tt@users.noreply.github.com>
2025-04-14 23:29:39 -0700

    feat: add command to get info abt each credit card

---
+++ commands/getCardInfo.ts
@@ -0,0 +1,37 @@
+import { SlashCommandBuilder, } from "@discordjs/builders";
+import { ChatInputCommandInteraction } from "discord.js";
+import { type CommandType, FISH, FLYINGFISH, SALMON } from "./utils.ts";
+ 
+
+export default {
+	data: new SlashCommandBuilder()
+		.setName('getcard')
+		.setDescription('Get all card info'),
+
+	/**
+	 * Reply with category reward % for all cards
+     * @param {ChatInputCommandInteraction} interaction
+	 */
+	async execute(interaction: ChatInputCommandInteraction) {
+        const cards = {
+            "fish": FISH, 
+            "flying fish": FLYINGFISH, 
+            "salmon": SALMON
+        };
+
+        let cardInfo = "";
+
+        // each card and each category in the card, append info to cardInfo
+        for(const [card, cardCategory] of Object.entries(cards)) {
+            cardInfo += `## ${card}\n`;
+
+            for(const [category, percent] of Object.entries(cardCategory)) {
+                cardInfo += `- ${category}: ${percent}%\n`
+            };
+
+            cardInfo += "-----------\n";
+        };
+
+		await interaction.reply(cardInfo);
+	},
+} as CommandType;
