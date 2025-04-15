7ad993bab395e3a801be34cebfadcc2bf0a3d0c3
mrat <78581772+mr4tt@users.noreply.github.com>
2025-04-14 23:28:45 -0700

    feat: add card info

---
+++ commands/utils.ts
@@ -0,0 +1,61 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import dotenv from 'dotenv';
+
+dotenv.config(); 
+
+export type CommandType = {
+	// contains data about the command, such as name and cmd description
+	data: SlashCommandBuilder;
+
+	/**
+	 * Executes a Discord slash command
+	 * @param {ChatInputCommandInteraction} interaction 
+	 */
+	execute: (interaction: ChatInputCommandInteraction) => Promise<void>; 
+};
+
+const quarterly: string = process.env.quarterly!;
+export const CATEGORIES = [
+    "dining", 
+    "delivery", 
+    "cafe", 
+    "bar",
+    "bakery",
+    "drugstore",
+    "entertainment", 
+    "concerts", 
+    {quarterly},
+    "grocery",
+    "other"
+];
+
+export const FISH = {
+    "dining": 3,
+    "delivery": 3,
+    "cafe": 3, 
+    "bar": 3,
+    "drugstore": 3,
+    "other": 1.5
+};
+
+export const FLYINGFISH = {
+    [quarterly]: 5,
+    "dining": 3,
+    "delivery": 3,
+    "cafe": 3, 
+    "bar": 3,
+    "drugstore": 3,
+    "other": 1
+};
+
+export const SALMON = {
+    "dining": 3, 
+    "delivery": 3, 
+    "cafe": 3, 
+    "bar": 3,
+    "bakery": 3,
+    "entertainment": 3, 
+    "concerts": 3, 
+    "grocery": 3,
+    "other": 1
+};
